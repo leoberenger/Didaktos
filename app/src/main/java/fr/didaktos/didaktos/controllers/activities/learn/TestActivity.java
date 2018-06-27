c5166bec1e7b501d46a9e89077ee2ca79d090665
leoberenger <leoberenger@gmail.com>
2018-06-27 17:31:54 +0200

    Improved Test Fragment UI

--- app/src/main/java/fr/didaktos/didaktos/controllers/activities/learn/TestActivity.java
+++ app/src/main/java/fr/didaktos/didaktos/controllers/activities/learn/TestActivity.java
@@ -2,10 +2,13 @@ package fr.didaktos.didaktos.controllers.activities.learn;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,16 +21,19 @@ import fr.didaktos.didaktos.injections.ViewModelFactory;
 import fr.didaktos.didaktos.models.Card;
 import fr.didaktos.didaktos.views.DeckViewModel;
 
-public class TestActivity extends BaseLearnActivity implements View.OnClickListener{
+import static android.view.View.GONE;
+
+public class TestActivity extends BaseLearnActivity
+        implements View.OnClickListener, EditText.OnEditorActionListener{
 
     private String TAG = "TestActivity";
 
     //Database
     private DeckViewModel deckViewModel;
 
-    private Button checkAnswerBtn;
     private EditText answerEditText;
     private TextView answerTextView;
+    private TextView wrongAnswerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -42,61 +48,84 @@ public class TestActivity extends BaseLearnActivity implements View.OnClickListe
 
     @Override
     protected void configureAnswer() {
-        checkAnswerBtn = (Button)findViewById(R.id.test_check);
-        checkAnswerBtn.setOnClickListener(this);
-        checkAnswerBtn.setVisibility(View.VISIBLE);
+        answerTextView = (TextView) findViewById(R.id.test_answer_txt);
+        answerTextView.setVisibility(GONE);
 
-        answerTextView = (TextView)findViewById(R.id.test_answer_txt);
-        answerTextView.setVisibility(View.INVISIBLE);
+        wrongAnswerTextView = (TextView)findViewById(R.id.test_user_wrong_answer_txt);
+        wrongAnswerTextView.setVisibility(GONE);
 
-        answerEditText = (EditText)findViewById(R.id.test_answer);
-        answerEditText.setBackgroundColor(0);
+        answerEditText = (EditText) findViewById(R.id.test_answer);
+        answerEditText.setVisibility(View.VISIBLE);
         answerEditText.setText("");
+        answerEditText.setOnEditorActionListener(this);
 
         nextFab.setOnClickListener(this);
-        nextFab.setVisibility(View.INVISIBLE);
+        nextFab.setVisibility(View.GONE);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.test_check :
-                checkAnswerBtn.setVisibility(View.INVISIBLE);
-                nextFab.setVisibility(View.VISIBLE);
-
-                String correctAnswer = deck.getCards().get(cardNumber).getValue();
-
-                if(answerEditText.getText().toString().equals(correctAnswer)){
-                    answerEditText.setBackgroundColor(Color.GREEN);
-
-                    Card successfulCard = deck.getCards().get(cardNumber);
-                    successfulCard.setStatus(2);
-                    successfulCard.setDeckId(deck.getId());
-                    //Update status in database
-                    this.deckViewModel.updateCard(successfulCard);
-                }else {
-                    answerEditText.setBackgroundColor(Color.RED);
-                    answerTextView.setVisibility(View.VISIBLE);
-                    answerTextView.setText(correctAnswer);
-                    deck.getCards().get(cardNumber).setStatus(0);
-                }
-
-                break;
-
+        switch (v.getId()) {
             case R.id.fab:
-                if(cardNumber>0){
+                if (cardNumber > 0) {
                     showNextCard();
-                }else{
+                } else {
                     endOfDeck();
                 }
                 break;
         }
     }
 
-    private void configureViewModel(){
+    private void configureViewModel() {
         ViewModelFactory viewModelFactory = Injection.provideViewModelFactory(this);
         this.deckViewModel = ViewModelProviders.of(this, viewModelFactory)
                 .get(DeckViewModel.class);
         this.deckViewModel.init();
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                actionId == EditorInfo.IME_ACTION_DONE ||
+                event != null &&
+                event.getAction() == KeyEvent.ACTION_DOWN &&
+                event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+
+            if (event == null || !event.isShiftPressed()) {
+
+                nextFab.setVisibility(View.VISIBLE);
+
+                String correctAnswer = deck.getCards().get(cardNumber).getValue();
+
+                if (answerEditText.getText().toString().equals(correctAnswer)) {
+
+                    answerEditText.setVisibility(GONE);
+
+                    answerTextView.setVisibility(View.VISIBLE);
+                    String goodAnswer = correctAnswer + " V";
+                    answerTextView.setText(goodAnswer);
+
+                    //Update status in database
+                    Card successfulCard = deck.getCards().get(cardNumber);
+                    successfulCard.setStatus(2);
+                    successfulCard.setDeckId(deck.getId());
+                    deckViewModel.updateCard(successfulCard);
+
+                } else {
+                    answerEditText.setVisibility(GONE);
+                    wrongAnswerTextView.setVisibility(View.VISIBLE);
+                    String wrongAnswer = answerEditText.getText().toString() + " X";
+                    wrongAnswerTextView.setText(wrongAnswer);
+
+                    answerTextView.setVisibility(View.VISIBLE);
+                    answerTextView.setText(correctAnswer);
+
+                    deck.getCards().get(cardNumber).setStatus(0);
+                }
+
+                return true; // consume.
+            }
+        }
+        return false; // pass on to other listeners.
     }
 }
